ef8da0f3c2cc106e7cccfaefb0bcf0f8a283f0e8
Yan <athenyan74@hotmail.com>
2020-04-29 10:13:25 +0200

    page gps

--- index.php
+++ index.php
@@ -15,53 +15,12 @@
 <h2>PDF explicatif de l'API:</p>
 
     
-    <p><a href="https://www.prevision-meteo.ch/uploads/pdf/recuperation-donnees-meteo.pdf"></a></p>
+<p><a href="https://www.prevision-meteo.ch/uploads/pdf/recuperation-donnees-meteo.pdf"></a></p>
 
 <button>Voir la météo sur ma position</button>
     
-    <script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
-    <script>
     
-        // Si le bouton est cliqué
-        $('button').click(function(){
-            
-            // Options de la geolocalisation
-            let options = {
-                enableHighAccuracy: true,       // Activation de la haute précision
-                timeout: 5000,                  // Temps en ms avant timeout
-                maximumAge: 0                   // Desactive le cache gps
-            }
-
-            // Fonction qui sera appelée si la localisation n'a pas pu être récupérée (e.code contient le code de l'erreur)
-            let error = function(e){
-                if(e.code == e.TIMEOUT){
-                    alert('Temps expiré')
-                } else if(e.code == e.PERMISSION_DENIED){
-                    alert('Vous avez refusé le geolocalisation');
-                } else if(e.code == e.POSITION_UNAVAILABLE){
-                    alert('Localisation impossible');
-                } else {
-                    alert('Problème inconnu');
-                }
-            }
-
-            // Fonction qui sera appelée si la localisation a reussi (p contient les coordonnées de localisation)
-            let success = function(p){
-
-                let latitude = p.coords.latitude;
-                let longitude = p.coords.longitude;
-
-                console.log('Votre latitude actuelle est ' + latitude + ' et votre longitude est ' + longitude);
-
-            }
-
-            // Code permettant de mettre en place la demande de geolocalisation au navigateur
-            navigator.geolocation.getCurrentPosition(success, error, options);
-
-        });
-    
-    </script>
-
+  
 
 <div id="view">
 </div>
